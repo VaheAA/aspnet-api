3cbf9555163eceebc7fbecdb4c840a1380a82a85
Vahe <vahe.abovyan@teampay.am>
2026-07-17 14:20:57 +0400

    add genre post endpoint

--- Endpoints/GenresEndpoints.cs
+++ Endpoints/GenresEndpoints.cs
@@ -1,6 +1,6 @@
-using Microsoft.EntityFrameworkCore;
 using GameStore.Api.Data;
 using GameStore.Api.Dtos;
+using Microsoft.EntityFrameworkCore;
 
 namespace GameStore.Api.Endpoints;
 
@@ -17,6 +17,18 @@ public static class GenresEndpoints
                     .Genres.Select(g => new GenreDto(g.Id, g.Name))
                     .AsNoTracking()
                     .ToListAsync()
+        );
+
+        group.MapPost(
+            "/",
+            async (GenreDto genreDto, GameStoreContext dbContext) =>
+            {
+                Genre genre = new() { Name = genreDto.Name };
+                dbContext.Genres.Add(genre);
+                await dbContext.SaveChangesAsync();
+
+                return Results.Created($"/genres/{genre.Id}", genreDto);
+            }
         );
     }
 }
